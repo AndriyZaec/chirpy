7bc57ef01e2696b6a7afefccec1badfb08111dfb
Andrew Zaiets <andrew.zaec@gmail.com>
2026-02-13 13:10:14 +0200

    Added gitignore; Added sqlc; Added goose; Added Create User;

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/andriyzaec/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
+	database       *database.Queries
 	fileserverHits atomic.Int32
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Cannot connect to DB")
+	}
+
+	dbQueries := database.New(db)
+
 	mux := http.NewServeMux()
 
-	apiCfg := &apiConfig{}
+	apiCfg := &apiConfig{
+		database: dbQueries,
+	}
 
 	fileServer := apiCfg.middlewareMetricsIn(http.FileServer(http.Dir(".")))
 	mux.Handle("/app", http.StripPrefix("/app", fileServer))
