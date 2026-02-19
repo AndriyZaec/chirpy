421ec3b4680bc59df98c596392c17460cd3cec85
Andrew Zaiets <andrew.zaec@gmail.com>
2026-02-19 14:26:02 +0200

    Added JWT creating & matching; Added tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,8 +2,10 @@ package auth_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/andriyzaec/chirpy/internal/auth"
+	"github.com/google/uuid"
 )
 
 func TestHashAndCheckPassword_Success(t *testing.T) {
@@ -40,5 +42,38 @@ func TestHashAndCheckPassword_WrongPassword(t *testing.T) {
 
 	if ok {
 		t.Fatal("expected password NOT to match hash, but it did")
+	}
+}
+
+func TestJWT_Success(t *testing.T) {
+	supersecret := "supersecret"
+	uuid, _ := uuid.NewUUID()
+	token, err := auth.MakeJWT(uuid, supersecret, 60*time.Second)
+	if err != nil {
+		t.Fatalf("unexpected error while creating token: %v", err)
+	}
+
+	claimsID, err := auth.ValidateJWT(token, supersecret)
+	if err != nil {
+		t.Fatalf("unexpected error while validating JWT: %v", err)
+	}
+
+	if claimsID != uuid {
+		t.Fatal("expected claimsId = uuid, not fulfilled")
+	}
+}
+
+func TestJWT_BadSecret(t *testing.T) {
+	supersecret := "supersecret"
+	uuid, _ := uuid.NewUUID()
+	token, err := auth.MakeJWT(uuid, supersecret, 60*time.Second)
+	if err != nil {
+		t.Fatalf("unexpected error while creating token: %v", err)
+	}
+
+	badSecret := "bad_secret"
+	_, err = auth.ValidateJWT(token, badSecret)
+	if err == nil {
+		t.Fatal("No error when bad secret is using, should be error")
 	}
 }
